e122337a1341db360151abfafcf79783810613f3
Rohan Batra <rohanbatra24@gmail.com>
2020-06-29 17:45:13 -0400

    add layout view to add product view

--- views/addproduct.js
+++ views/addproduct.js
@@ -1,8 +1,47 @@
+const layout = require('./layout');
+
 module.exports = function(categories) {
 	const categoriesList = categories.map((category) => {
 		return `
     <option value="${category}">${category}</option>`;
 	});
+
+	return layout(`
+  
+<body class="d-flex flex-column align-items-center">
+  <h1>ADMIN PANEL</h1>
+  <h3 class="text-info">Add a new product</h3>
+
+  
+  <form class='w-50'method='post' action='/admin/addproduct'>
+
+ 
+
+    <div class='form-group'>
+    <label for="product">Product Title</label>
+    <input type="text" name='title' class="form-control" placeholder="">
+    </div>
+
+    <div class="form-group">
+    <label for="price">Price (CAD)</label>
+    <input type="number" name='price' class="form-control" id="price" placeholder="">
+    </div>
+
+    <div class="form-group">
+    <label for="category">Category</label>
+    <select name="category" id="category">
+    <option value="" disabled selected>Select one..</option>
+    ${categoriesList}
+    </select>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+    
+  </form>
+  
+    
+</body>
+  `);
 
 	return `
     <!DOCTYPE html>
@@ -34,11 +73,11 @@ module.exports = function(categories) {
   
 </header>
 
-<h1>ADMIN PANEL</h1>
 
-<h3 class="text-info">Add a new product</h3>
     
   <body class="d-flex flex-column align-items-center">
+  <h1>ADMIN PANEL</h1>
+  <h3 class="text-info">Add a new product</h3>
   <form method='post' action='/admin/addproduct' class="w-50">
   <div class="form-group">
     <label for="product">Product Title</label>
