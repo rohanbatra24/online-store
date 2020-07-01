f53e2da055ee03c6ddf0e6da9a3bafeef8bdf42a
Rohan Batra <rohanbatra24@gmail.com>
2020-07-01 12:53:43 -0400

    require crypto and util (promisify)

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const crypto = require('crypto');
+
+const util = require('util'); // to promisify scrypt result
+
+const scrypt = util.promisify(crypto.scrypt);
+
 const login = require('../views/auth/login');
 
 const signup = require('../views/auth/signup');
@@ -9,12 +15,11 @@ const signup = require('../views/auth/signup');
 const { db } = require('./db/database');
 
 router.get('/login', (req, res) => {
-	res.send(login(req.cookies.userName));
+	res.send(login(req.session.userName));
 });
 
 router.get('/logout', (req, res) => {
-	res.clearCookie('userId');
-	res.clearCookie('userName');
+	req.session = null;
 
 	res.redirect('/');
 });
@@ -22,12 +27,11 @@ router.get('/logout', (req, res) => {
 router.post('/login', (req, res) => {
 	db.select('*').table('users').where({ email: req.body.email, password: req.body.password }).then((data) => {
 		if (data.length) {
-			res.cookie('userId', data[0].id);
-			res.cookie('userName', data[0].name);
+			req.session = { userId: data[0].id, userName: data[0].name };
 			res.redirect('/');
 		}
 		else {
-			res.send('wrong credentials');
+			res.send('User email and password do not match');
 		}
 	});
 });
@@ -36,15 +40,22 @@ router.get('/signup', (req, res) => {
 	res.send(signup());
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
 	const { name, email, password } = req.body;
 
-	db('users').insert({ name: name, email: email, password: password }).returning('*').then((data) => {
-		res.cookie('userId', data[0].id);
-		res.cookie('userName', data[0].name);
+	const salt = crypto.randomBytes(8).toString('hex');
 
-		res.redirect('/');
-	});
+	//scrypt returns a promise (dont need callback) because of util promisify at the top
+	const hashedBuffer = await scrypt(password, salt, 64);
+
+	db('users')
+		.insert({ name: name, email: email, password: `${hashedBuffer.toString('hex')}.${salt}` })
+		.returning('*')
+		.then((data) => {
+			req.session = { userId: data[0].id, userName: data[0].name };
+			res.redirect('/');
+		})
+		.catch((err) => console.log('err', err));
 });
 
 module.exports = router;
