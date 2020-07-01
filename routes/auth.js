d79dc7afecd12a74438c8d5df73708dedefbf5bf
Rohan Batra <rohanbatra24@gmail.com>
2020-07-01 10:36:38 -0400

    create sign up route

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const cart = require('../views/products/cart');
-
 const login = require('../views/auth/login');
+
+const signup = require('../views/auth/signup');
 
 const { db } = require('./db/database');
 
 router.get('/login', (req, res) => {
-	res.send(login(req.cookies.userId));
+	res.send(login(req.cookies.userName));
 });
 
 router.get('/logout', (req, res) => {
 	res.clearCookie('userId');
+	res.clearCookie('userName');
+
 	res.redirect('/');
 });
 
@@ -21,6 +23,7 @@ router.post('/login', (req, res) => {
 	db.select('*').table('users').where({ email: req.body.email, password: req.body.password }).then((data) => {
 		if (data.length) {
 			res.cookie('userId', data[0].id);
+			res.cookie('userName', data[0].name);
 			res.redirect('/');
 		}
 		else {
@@ -30,11 +33,20 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-	res.send(homepage(database.products));
+	res.send(signup());
 });
 
 router.post('/signup', (req, res) => {
-	res.send(homepage(database.products));
+	console.log('req.body', req.body);
+
+	const { name, email, password } = req.body;
+
+	db('users').insert({ name: name, email: email, password: password }).returning('*').then((data) => {
+		res.cookie('userId', data[0].id);
+		res.cookie('userName', data[0].name);
+
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
