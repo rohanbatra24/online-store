c9686f6b1e899a03104bc171d11746494e01051f
Rohan Batra <rohanbatra24@gmail.com>
2020-07-02 15:27:55 -0400

    remove log in requirement for home page

--- routes/products.js
+++ routes/products.js
@@ -11,12 +11,7 @@ const categories = require('../views/products/categories');
 const categoryView = require('../views/products/categoryView');
 
 router.get('/', (req, res) => {
-	if (req.session.userId) {
-		db.select('*').table('products').then((products) => res.send(homepage(req.session.userName, products)));
-	}
-	else {
-		res.redirect('/login');
-	}
+	db.select('*').table('products').then((products) => res.send(homepage(req.session.userName, products)));
 });
 
 router.get('/categories', (req, res) => {
