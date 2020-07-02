4efa29e7ad2c4598cca79bd5c2b5241690abddc4
Rohan Batra <rohanbatra24@gmail.com>
2020-07-02 15:29:45 -0400

    add login validation logic

--- routes/validators.js
+++ routes/validators.js
@@ -2,13 +2,19 @@ const { check } = require('express-validator');
 
 const { db } = require('./db/database');
 
+const util = require('util'); // to promisify scrypt result
+
+const crypto = require('crypto');
+
+const scrypt = util.promisify(crypto.scrypt);
+
 module.exports = {
 	requireName                 : check('name').notEmpty().withMessage('Name cannot be empty'),
 	requireEmail                : check('email')
 		.trim()
 		.normalizeEmail()
 		.isEmail()
-		.withMessage('Must be a valid email')
+		.withMessage('Please provide a valid email')
 		.custom(async (email) => {
 			const exists = await db('users').where({ email: email }).select('*');
 
@@ -31,5 +37,33 @@ module.exports = {
 			else {
 				return true;
 			}
-		})
+		}),
+	requireEmailExists          : check('email')
+		.trim()
+		.normalizeEmail()
+		.isEmail()
+		.withMessage('Must provide a valid email')
+		.custom(async (email) => {
+			const user = await db('users').select('*').where({ email: email });
+			if (!user.length) {
+				throw new Error('Email not found');
+			}
+		}),
+	requireValidPassword        : check('password').trim().custom(async (password, { req }) => {
+		const comparePasswords = async (dbPass, supplied) => {
+			const [ hashed, salt ] = dbPass.split('.');
+			const suppliedHashedBuf = await scrypt(supplied, salt, 64);
+			return hashed === suppliedHashedBuf.toString('hex');
+		};
+
+		const user = await db.select('*').table('users').where({ email: req.body.email });
+
+		if (!user[0]) {
+			throw new Error('Invalid Password');
+		}
+
+		if ((await comparePasswords(user[0].password, password)) === false) {
+			throw new Error('Invalid password');
+		}
+	})
 };
