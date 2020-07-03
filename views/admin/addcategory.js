d2d9b1d83e71c0b6d58ef16943edf34e1e92aeb8
Rohan Batra <rohanbatra24@gmail.com>
2020-07-03 09:07:16 -0400

    add helper function to display error

--- views/admin/addcategory.js
+++ views/admin/addcategory.js
@@ -1,11 +1,19 @@
 const layout = require('../layout');
 
-module.exports = function(user) {
+module.exports = function(user, error) {
+	const accessError = (error) => {
+		if (error) {
+			return `<p class='text-danger m-3'>Sorry! You do not have admin access. Please log in with the admin account.</p>`;
+		}
+		else return '';
+	};
+
 	return layout(
 		user,
 		`
+    ${accessError(error)}
+
   <body class="d-flex flex-column align-items-center">
-    
     <form method='post' action='/admin/addCategory' class="w-50">
   <div class="form-group">
     <label for="product">Category Name</label>
