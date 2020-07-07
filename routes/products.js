77c935fa378a6b31322af667e93c7abe8e1932c0
Rohan Batra <rohanbatra24@gmail.com>
2020-07-07 11:57:29 -0400

    update db query to include category name

--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,10 @@ const categories = require('../views/products/categories');
 const categoryView = require('../views/products/categoryView');
 
 router.get('/', (req, res) => {
-	db.select('*').table('products').then((products) => res.send(homepage(req.session.userName, products)));
+	db.select('*').table('products').join('categories', 'categories.id', '=', 'category_id').then((products) => {
+		console.log('products', products);
+		res.send(homepage(req.session.userName, products));
+	});
 });
 
 router.get('/categories', (req, res) => {
