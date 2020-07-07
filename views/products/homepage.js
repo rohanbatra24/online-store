92202e5bba016f20218c295468f37a9ca96f9b22
Rohan Batra <rohanbatra24@gmail.com>
2020-07-07 11:57:58 -0400

    update homepage card layout

--- views/products/homepage.js
+++ views/products/homepage.js
@@ -6,7 +6,7 @@ module.exports = function(user, products) {
 			return `
     <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center h-100">
         <img src="${product.image}" class="db w-100 br2 br--top" alt="">
-        <div class="pa2 ph3-ns pb3-ns">
+        <div class="pa2">
           <div class="dt w-100 mt1">
             <div class="dtc">
               <h1 class="f5 f4-ns mv0">${product.title}</h1>
@@ -19,10 +19,15 @@ module.exports = function(user, products) {
             If it fits, i sits burrow under covers. Destroy couch leave hair everywhere,
             and touch water with paw then recoil in horror.
           </p>
+
+          <div class="">
+              <h2 class="f5 text-muted"> Category: ${product.name}</h2>
+            </div>
           <div class="ph3 mb4">
-        <form method='POST' action='/addtocart/${product.id}'>
-        <button type='submit' class="f6 grow no-underline br-pill ba bw2 ph3 pv2 mb2 dib dark-green">Add to Cart</button>
-        </form>
+          <form method='POST' action='/addtocart/${product.id}'>
+          <button type='submit' class="f6 grow no-underline br-pill ba bw2 ph3 pv2 dib dark-green mt-3">Add to Cart</button>
+          </form>
+          
         </div>
         </div>
         
