f6a1de1447a9450686a4d57f765053e0b8ad38da
Rohan Batra <rohanbatra@MacBook-Air.local>
2021-01-25 18:45:44 -0500

    add procfile

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000
+
 const cors = require('cors');
 
 const bodyParser = require('body-parser');
@@ -42,6 +44,6 @@ app.use(cartRouter);
 
 app.use(adminRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log('App listening on port 3000!');
 });
