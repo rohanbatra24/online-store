cc8bc64f8ab1aa17307b8cecdbaed30db6748c30
Rohan Batra <rohanbatra@MacBook-Air.local>
2021-01-25 21:34:55 -0500

    remove routes for heroku test

--- routes/products.js
+++ routes/products.js
@@ -1,45 +1,43 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const { db } = require('./db/database');
+const { db } = require("./db/database");
 
-const homepage = require('./views/products/homepage');
+const homepage = require("./views/products/homepage");
 
-const categories = require('./views/products/categories');
+const categories = require("./views/products/categories");
 
-const categoryView = require('./views/products/categoryView');
+const categoryView = require("./views/products/categoryView");
 
-router.get('/', (req, res) => {
-	// db
-	// 	.select('*', 'products.id', 'products.image')
-	// 	.table('products')
-	// 	.join('categories', 'categories.id', '=', 'category_id')
-	// 	.then((products) => {
-	// 		res.send(homepage(req.session.userName, products));
-	// 	});
+router.get("/", (req, res) => {
+  // db
+  // 	.select('*', 'products.id', 'products.image')
+  // 	.table('products')
+  // 	.join('categories', 'categories.id', '=', 'category_id')
+  // 	.then((products) => {
+  // 		res.send(homepage(req.session.userName, products));
+  // 	});
 
-		res.send(`<h1>hello world</h1>`);
+  res.send(`<h1>hello world</h1>`);
 });
 
+// router.get('/categories', (req, res) => {
+// 	db
+// 		.select('*')
+// 		.table('categories')
+// 		.then((categoriesList) => res.send(categories(req.session.userName, categoriesList)));
+// });
 
+// router.get('/categories/:id', (req, res) => {
+// 	catId = req.params.id;
 
-router.get('/categories', (req, res) => {
-	db
-		.select('*')
-		.table('categories')
-		.then((categoriesList) => res.send(categories(req.session.userName, categoriesList)));
-});
-
-router.get('/categories/:id', (req, res) => {
-	catId = req.params.id;
-
-	db
-		.select('*')
-		.table('categories')
-		.join('products', 'categories.id', '=', 'category_id')
-		.where({ category_id: catId })
-		.then((products) => res.send(categoryView(req.session.userName, products)));
-});
+// 	db
+// 		.select('*')
+// 		.table('categories')
+// 		.join('products', 'categories.id', '=', 'category_id')
+// 		.where({ category_id: catId })
+// 		.then((products) => res.send(categoryView(req.session.userName, products)));
+// });
 
 module.exports = router;
