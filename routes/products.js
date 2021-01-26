cbb563473417358714e066ae9613faf4aad32ce5
Rohan Batra <rohanbatra@MacBook-Air.local>
2021-01-25 23:02:39 -0500

    add db

--- routes/products.js
+++ routes/products.js
@@ -11,33 +11,30 @@ const categories = require(__dirname + "/../views/products/categories");
 const categoryView = require(__dirname + "/../views/products/categoryView");
 
 router.get("/", (req, res) => {
-  // db
-  // 	.select('*', 'products.id', 'products.image')
-  // 	.table('products')
-  // 	.join('categories', 'categories.id', '=', 'category_id')
-  // 	.then((products) => {
-  // 		res.send(homepage(req.session.userName, products));
-  // 	});
-
-  res.send(`<h1>hello world</h1>`);
+  db.select("*", "products.id", "products.image")
+    .table("products")
+    .join("categories", "categories.id", "=", "category_id")
+    .then((products) => {
+      res.send(homepage(req.session.userName, products));
+    });
 });
 
-// router.get('/categories', (req, res) => {
-// 	db
-// 		.select('*')
-// 		.table('categories')
-// 		.then((categoriesList) => res.send(categories(req.session.userName, categoriesList)));
-// });
+router.get("/categories", (req, res) => {
+  db.select("*")
+    .table("categories")
+    .then((categoriesList) =>
+      res.send(categories(req.session.userName, categoriesList))
+    );
+});
 
-// router.get('/categories/:id', (req, res) => {
-// 	catId = req.params.id;
+router.get("/categories/:id", (req, res) => {
+  catId = req.params.id;
 
-// 	db
-// 		.select('*')
-// 		.table('categories')
-// 		.join('products', 'categories.id', '=', 'category_id')
-// 		.where({ category_id: catId })
-// 		.then((products) => res.send(categoryView(req.session.userName, products)));
-// });
+  db.select("*")
+    .table("categories")
+    .join("products", "categories.id", "=", "category_id")
+    .where({ category_id: catId })
+    .then((products) => res.send(categoryView(req.session.userName, products)));
+});
 
 module.exports = router;
