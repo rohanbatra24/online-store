89e3956ec06fb0a31f97e5aa6a8a56ba5f92792e
Rohan Batra <rohanbatra24@gmail.com>
2021-01-31 16:29:03 -0500

    add temp content for about us and locations pages

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const layout = require("../views/layout");
 
 const router = express.Router();
 
 router.get("/about", (req, res) => {
-  res.send(`<h4>Page under construction!</h4>`);
+  res.send(
+    layout(
+      req.session.userName,
+      `<h4 class='aboutPageContent'>Page under construction!</h4><img class='underConstructionImg'src='https://www.pngkit.com/png/detail/208-2087380_construction-icon-under-construction.png'/>`
+    )
+  );
 });
 
 module.exports = router;
--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
+const layout = require("../views/layout");
 
 router.get("/contact", (req, res) => {
-  res.send(`<h4>Page under construction!</h4>`);
+  res.send(
+    layout(
+      req.session.userName,
+      `<h4 class='locationsPageContent'>Page under construction!</h4><img class='underConstructionImg'src='https://www.pngkit.com/png/detail/208-2087380_construction-icon-under-construction.png'/>`
+    )
+  );
 });
 
 module.exports = router;
