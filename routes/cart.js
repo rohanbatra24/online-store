22ffca9438ac1ae1fa6929ae3dc3609496b50c3c
rohanbatra24 <rohanbatra24@gmail.com>
2021-02-14 23:30:33 -0500

    Add authentication check for add to cart

--- routes/cart.js
+++ routes/cart.js
@@ -46,61 +46,65 @@ router.post("/addtocart/:id", (req, res) => {
   // 1. put the id of the product at the end of the url. for eg. /addtocart/${product.id} and make this route /addtocart/:id & read with req.params.id
   // 2. add a hidden input element inside the form and give it a value attr of ${product.id} and name attr of productID, then you can read that info from inside the route handler with req.body.productID
 
-  const productId = req.params.id;
+  if (req.session.userId) {
+    const productId = req.params.id;
 
-  const userId = req.session.userId;
+    const userId = req.session.userId;
 
-  let cartId;
+    let cartId;
 
-  db("cart")
-    .where({ user_id: userId })
-    .then((data) => {
-      if (!data.length) {
-        db("cart")
-          .insert({ user_id: userId, datecreated: new Date() })
-          .returning("*")
+    db("cart")
+      .where({ user_id: userId })
+      .then((data) => {
+        if (!data.length) {
+          db("cart")
+            .insert({ user_id: userId, datecreated: new Date() })
+            .returning("*")
+            .then((data) => {
+              cartId = data[0].id;
+            });
+        } else {
+          cartId = data[0].id;
+        }
+      })
+      .then(() => {
+        db("cartitems")
+          .where({
+            product_id: productId,
+            cart_id: cartId || 0,
+          })
           .then((data) => {
-            cartId = data[0].id;
-          });
-      } else {
-        cartId = data[0].id;
-      }
-    })
-    .then(() => {
-      db("cartitems")
-        .where({
-          product_id: productId,
-          cart_id: cartId || 0,
-        })
-        .then((data) => {
-          if (data.length > 0) {
-            db("cartitems")
-              .where({ product_id: productId, cart_id: cartId })
-              .increment("quantity", 1)
-              .then((data) => data)
-              .catch((err) => console.log("err", err));
-          } else {
-            db("products")
-              .where({
-                id: productId,
-              })
-              .select("id", "title", "price")
-              .then((data) => {
-                db("cartitems")
-                  .insert({
-                    cart_id: cartId,
-                    product_id: data[0].id,
-                    quantity: 1,
-                    price: data[0].price,
-                  })
-                  .catch((err) => console.log("err", err));
-              });
-          }
-        })
-        .catch((err) => console.log("err", err));
-    });
+            if (data.length > 0) {
+              db("cartitems")
+                .where({ product_id: productId, cart_id: cartId })
+                .increment("quantity", 1)
+                .then((data) => data)
+                .catch((err) => console.log("err", err));
+            } else {
+              db("products")
+                .where({
+                  id: productId,
+                })
+                .select("id", "title", "price")
+                .then((data) => {
+                  db("cartitems")
+                    .insert({
+                      cart_id: cartId,
+                      product_id: data[0].id,
+                      quantity: 1,
+                      price: data[0].price,
+                    })
+                    .catch((err) => console.log("err", err));
+                });
+            }
+          })
+          .catch((err) => console.log("err", err));
+      });
 
-  res.redirect("/");
+    res.redirect("/");
+  } else {
+    res.redirect("/cart");
+  }
 });
 
 module.exports = router;
