aa2da16f97caf2bf7882dac4b798a3cd435e062d
Rohan Batra <rohanbatra24@gmail.com>
2021-03-23 21:13:17 -0400

    add ssl config for heroku

--- routes/db/database.js
+++ routes/db/database.js
@@ -3,11 +3,13 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "ec2-52-3-4-232.compute-1.amazonaws.com",
-    user: "wcolmwychdvnks",
-    password:
-      "1ed027e47503ef655d779c66e5db1df11c98740b7081c0424cfb0dbf6051354b",
-    database: "dc4u844hrlijvr",
+    host: "localhost",
+    user: "rohanbatra",
+    password: "",
+    database: "ecommerce",
+  },
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
